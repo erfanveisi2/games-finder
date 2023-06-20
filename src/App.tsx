3aab04f3d3ec3b50e8fd8e33dc55d51422082139
erv <erfanveisi2@gmail.com>
2023-06-21 00:20:29 +0300

    API fetching completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import { type } from '@testing-library/user-event/dist/type';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component <any , any>{
+  constructor(){
+    super(arguments)
+
+    this.state={
+      games:[]
+    }
+  }
+
+  componentDidMount(): void {
+    fetch('https://api.rawg.io/api/platforms?key=660565b53b394a18b4d0769a3b527398')
+    .then(response=>response.json())
+    .then(response=>this.setState(()=> {return {games:response.results}} ,()=>{console.log(this.state.games)}))
+  }
+
+  render(){
+    return(
+      <div className="App">
+        
+      </div>
+    )
+  }
 }
 
 export default App;
